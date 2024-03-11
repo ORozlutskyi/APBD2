7fd14c724936e4590ac6853d1d8a52d47e44fd6d
s28398 <s28398@pjwstk.edu.pl>
2024-03-11 16:26:27 +0100

    Modyfikacja 1

--- APBD/APBD2/Program.cs
+++ APBD/APBD2/Program.cs
@@ -5,6 +5,7 @@ class Program
     {
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Modyfikacja 1");
+        Console.WriteLine("Modyfikacja 2");
                 
     }
 }
