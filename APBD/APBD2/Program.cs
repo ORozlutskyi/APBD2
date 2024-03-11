429127e0e2131bb91539bac969ae086b0013a9f0
s28398 <s28398@pjwstk.edu.pl>
2024-03-11 16:35:38 +0100

    metoda_max

--- APBD/APBD2/Program.cs
+++ APBD/APBD2/Program.cs
@@ -10,6 +10,9 @@ class Program
 
         int[] liczby = { 1, 2, 3, 4, 5 };
         Console.WriteLine(Srednia(liczby));
+        
+        int[] liczby2 = { 1, 2, 3, 4, 5, -2, 10, 6 };
+        Console.WriteLine(Max(liczby2));
     }
 
     public static int Srednia(int[] liczby)
@@ -21,5 +24,16 @@ class Program
         }
 
         return suma / liczby.Length;
+    }
+    
+    public static int Max(int[] liczby)
+    {
+        int max = 0;
+        for (int i = 0; i < liczby.Length; i++)
+        {
+            if (max < liczby[i]) max = liczby[i];
+        }
+
+        return max;
     }
 }
